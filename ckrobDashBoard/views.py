aa9dcedccb0a18a7a4c990fd7490d0a749b3622b
saposki <saposki@gmail.com>
2016-02-02 00:53:54 -0800

    implemented home page view

--- ckrobDashBoard/views.py
+++ ckrobDashBoard/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 from django.http import HttpResponse
 
+def homePageView(request):
+    return HttpResponse('<html><body>Home</body></html>')
 def dashBoardView(request):
     return HttpResponse('<html><body>Dash Board</body></html>')
 def blogView(request):
