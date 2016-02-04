9fd71eb10facae184fd3b2b2fb78a8c3cd35dc22
saposki <saposki@gmail.com>
2016-02-04 15:57:41 -0800

    modified blog routes

--- ckrob/urls.py
+++ ckrob/urls.py
@@ -19,8 +19,8 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', 'ckrobDashBoard.views.homePageView'),
-    url(r'^home/$', 'ckrobDashBoard.views.homePageView'),
-    url(r'^dashboard/$', 'ckrobDashBoard.views.dashBoardView'),
-    url(r'^blog/$', 'ckrobDashBoard.views.blogView'),
+    # url(r'^$', 'ckrobDashBoard.views.homePageView'),
+    # url(r'^home/$', 'ckrobDashBoard.views.homePageView'),
+    # url(r'^dashboard/$', 'ckrobDashBoard.views.dashBoardView'),
+    url(r'^blog/$', 'blog.views.blogView'),
 ]
--- ckrobDashBoard/views.py
+++ ckrobDashBoard/views.py
@@ -6,5 +6,3 @@ def homePageView(request):
     return HttpResponse('<html><body>Home</body></html>')
 def dashBoardView(request):
     return HttpResponse('<html><body>Dash Board</body></html>')
-def blogView(request):
-    return HttpResponse('<html><body>Blog</body></html>')
